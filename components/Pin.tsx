214aa4cec778cca0ea6c5c165dfe9a144890413c
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2022-05-10 16:27:43 +0900

    Masonry Layout

--- components/Pin.tsx
+++ components/Pin.tsx
@@ -32,7 +32,9 @@ const Pin = (props) => {
         </Pressable>
       </View>
 
-      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title} numberOfLines={2}>
+        {title}
+      </Text>
     </View>
   );
 };
@@ -41,17 +43,20 @@ export default Pin;
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    margin: 10,
+    fontSize: 16,
+    lineHeight: 22,
+    fontWeight: "600",
+    margin: 5,
+    color: "#181818",
   },
   image: {
     width: "100%",
-    borderRadius: 25,
+    borderRadius: 15,
     aspectRatio: 1 / 1,
   },
   pin: {
     width: "100%",
+    padding: 4,
   },
   heartBtn: {
     backgroundColor: "#d3cfd4",
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Image, ScrollView } from "react-native";
+import { StyleSheet, Image, ScrollView, FlatList } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import Pin from "../components/Pin";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import pins from "../assets/data/pins";
 
 export default function HomeScreen({
   navigation,
@@ -11,13 +12,20 @@ export default function HomeScreen({
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Pin
-          pin={{
-            title: "Title",
-            image:
-              "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg",
-          }}
-        />
+        <View style={styles.column}>
+          {pins
+            .filter((_, index) => index % 2 === 0)
+            .map((pin) => (
+              <Pin pin={pin} key={pin.id} />
+            ))}
+        </View>
+        <View style={styles.column}>
+          {pins
+            .filter((_, index) => index % 2 !== 0)
+            .map((pin) => (
+              <Pin pin={pin} key={pin.id} />
+            ))}
+        </View>
       </View>
     </ScrollView>
   );
@@ -25,9 +33,10 @@ export default function HomeScreen({
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     padding: 10,
+    flexDirection: "row",
+  },
+  column: {
+    flex: 1,
   },
 });
